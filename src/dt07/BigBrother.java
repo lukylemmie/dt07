2f7ff5e1fc93f31783bb0fbd487f513f482affab
Lem <lemmie6@gmail.com>
2012-04-23 12:18:10 +1000

    Reading of .txt file and censorship almost set up

---
+++ src/dt07/BigBrother.java
@@ -0,0 +1,48 @@
+package dt07;
+
+import javax.security.auth.login.FailedLoginException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Andrew
+ * Date: 23/04/12
+ * Time: 11:24 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BigBrother {
+    private Hashtable<String, String> censor = new Hashtable<String, String>();
+    private TextFileReader reader = new TextFileReader();
+    private ArrayList<String> censorWords;
+
+    public BigBrother(){
+        String wordPair[];
+
+        try {
+            censorWords = reader.OpenFile("censorWordList.txt");
+        } catch (IOException e) {
+            System.out.println("Failed to open censorWordList.txt file.  Please make sure it is in active directory");
+        }
+
+        for(String line : censorWords){
+            wordPair = line.split(",");
+            censor.put(wordPair[0], wordPair[1]);
+        }
+    }
+
+    public String CensorText(String input){
+        String output = "";
+        String[] words;
+        String temp;
+
+        words = input.split(" ");
+
+        for(String word : words){
+            temp = word.replaceAll("[^a-zA-Z0-9]","");
+        }
+
+        return output;
+    }
+}
